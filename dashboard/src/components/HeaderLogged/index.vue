c2abab1a17cb36be6e0082e4a196a09f2470931b
Emerson <eemr3@yahoo.com.br>
2021-03-04 18:26:49 -0300

    test: component HeaderLogged

--- dashboard/src/components/HeaderLogged/index.vue
+++ dashboard/src/components/HeaderLogged/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="flex items-center justify-between w-4/5 max-w-6xl py-10">
-		<dir class="w-28 lg:w-36">
+		<div class="w-28 lg:w-36">
 			<img class="w-full" src="../../assets/images/logo_white.png" alt="Logo" />
-		</dir>
+		</div>
 		<div class="flex">
 			<ul class="flex list-none">
 				<li
@@ -18,6 +18,7 @@
 					Feedbacks
 				</li>
 				<li
+					id="logout-button"
 					@click="handleLogout"
 					class="px-6 py-2 font-bold bg-white rounded-full text-brand-main cursor-pointer focus:outline-none"
 				>
@@ -33,29 +34,22 @@
 	import { useRouter } from 'vue-router'
 	import useStore from '../../hooks/useStore'
 	import { clearCurrentUser } from '../../store/user'
-
 	export default {
 		setup() {
 			const router = useRouter()
 			const store = useStore('User')
-
 			const logoutLabel = computed(() => {
 				if (!store.currentUser.name) {
 					return '...'
 				}
 				return `${store.currentUser.name} (sair)`
 			})
-
 			function handleLogout() {
 				window.localStorage.removeItem('token')
 				clearCurrentUser()
 				router.push({ name: 'Home' })
 			}
-			return {
-				router,
-				logoutLabel,
-				handleLogout,
-			}
+			return { router, logoutLabel, handleLogout }
 		},
 	}
 </script>
